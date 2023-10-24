8615ce4bda834a636d00673d348dc96b2ffd3952
e-box <e-box@live.ru>
2023-10-24 21:28:58 +0800

    Fixed pagination

--- habits/paginators.py
+++ habits/paginators.py
@@ -3,4 +3,4 @@ from rest_framework.pagination import PageNumberPagination
 
 class DefaultPaginator(PageNumberPagination):
     """Класс реализует пагинацию"""
-    page_size = 10
+    page_size = 5
